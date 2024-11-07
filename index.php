fccd3418c0b33532c426ad285cbf581aa7fcb33d
Mehmet Reşit Akın <103512411+resitakinn@users.noreply.github.com>
2024-11-07 20:32:35 +0300

    Kosul-isleme

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <?php 
 $strings_variable = 'Mehmet Reşit Akın';
 $args1 = 20;
-$args2 = '20';
+$args2 = 20;
 
-if ($args1 == $args2) {
+if ($args1 === $args2) {
     print "Eşittir";
+}elseif ($args1 > $args2) {
+    print $args1 . " Büyüktür...";
+}
+elseif ($args1 < $args2) {
+    print $args1 . " Küçüktür...";
 }
 else {
-    print "Eşit Değil";
+    print "Böyle bir Koşul Yok";
 }
 
 
