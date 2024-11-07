9ebde0a9bbac6cff7a9a78c344b7c1b73ae6d35a
Mehmet Reşit Akın <103512411+resitakinn@users.noreply.github.com>
2024-11-07 21:53:34 +0300

    koşullu ifadeler

--- index.php
+++ index.php
@@ -1,29 +1,46 @@
-<?php 
+<?php
 $strings_variable = 'Mehmet Reşit Akın';
 $args1 = 20;
 $args2 = 20;
-
+print "<br>";
+print "<h3>Koşullu ifadeler</h3>" . "<br>";
 if ($args1 === $args2) {
     print "Eşittir";
-}elseif ($args1 > $args2) {
+} elseif ($args1 > $args2) {
     print $args1 . " Büyüktür...";
-}
-elseif ($args1 < $args2) {
+} elseif ($args1 < $args2) {
     print $args1 . " Küçüktür...";
-}
-else {
+} else {
     print "Böyle bir Koşul Yok";
 }
+print "<br>";
+print "<h3>Diziler</h3>" . "<br>";
 
 
+$array = [
+    ["product" => "Telefon", "Marka" => "Samsung", "color" => "Siyah"],
+    [ "product" => "Bilgisayar", "Marka" => "Huawei", "color" => "Siyah","İşlemci Tipi"=>["i7","RYZEN 7"] ],    
+    ["product" => "Tablet", "Marka" => "Huawei", "color" => "Siyah"]
+];
+print "<pre>";
+print_r($array);
+print "</pre>";
+print "<br>";
+print "<h3>Koşullu diziler 2</h3>" . "<br>";
 
 
+ foreach ($array as $key => $value) {
+  print   "Ürün Adı = ".$value['product']." ".$value['Marka']."<br>";
 
 
-
-
-
-
-
+ }
+ print "<h3>Koşullu diziler 3</h3>" . "<br>";
+ $array2 = [
+    "Bilgisayar",
+    "Telefon"
+ ];
+for ($i=0; $i < count($array2) ; $i++) { 
+    print  $array2[$i]."<br>";
+}
 
 ?>
