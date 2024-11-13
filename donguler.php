4fd52eba9434da18e7ad9f6101746cf923b6e01f
Mehmet Reşit Akın <103512411+resitakinn@users.noreply.github.com>
2024-11-13 22:29:43 +0300

    donguler-dosyası-olusturuldu-ve-üstünde-calısıldı

---
+++ donguler.php
@@ -0,0 +1,46 @@
+<?php
+
+
+
+
+
+
+// for ($i = 0; $i < count($products); $i++) {
+//     print $products[$i]['product_id'] . " | " . $products[$i]['name'] . "<hr>";
+// }
+
+//**eksilterek for dongüsü ayarlama */
+// for ($i = 3; $i > -1; $i--) {
+//     print $products[$i]['product_id'] . " | " . $products[$i]['name'] ;
+//     print "<hr/>";
+// }
+
+// foreach ($products as $key => $product) {
+//     print $product['product_id']. '<br>';
+//     print $product['name']. '<br>';
+//     print $product['color_code']. '<br>';
+//     print $product['color_name']. '<br>';
+//     print '<hr>';
+// }
+
+//? While Döngüsü */
+
+// $a = 0;
+// while ($a <= 2) {
+//     print $products[$a]['product_id'] . " " . $products[$a]['name'] . "<br>";
+//     print "<hr>";
+//     $a++;
+// }
+
+$products = [
+    ['product_id' => 1, 'name' => 'Bilgisayar', 'color_code' => 'SH', 'color_name' => 'Siyah'],
+    ['product_id' => 2, 'name' => 'Klavye', 'color_code' => 'BZ', 'color_name' => 'Beyaz'],
+    ['product_id' => 3, 'name' => 'Monitör', 'color_code' => 'KZ', 'color_name' => 'Kırmızı']
+];
+
+
+
+
+
+
+?>
