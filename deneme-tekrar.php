aac702e323a6143630a6813544e6fb194390de10
Mehmet Reşit Akın <103512411+resitakinn@users.noreply.github.com>
2024-11-13 21:08:13 +0300

    Sayfa Düzenlendi

--- deneme-tekrar.php
+++ deneme-tekrar.php
@@ -1,4 +1,122 @@
 <?php
+print SITE;
+print "<br>";
+print APP_ROOT;
+
+
+// $strings_variable = 'Mehmet Reşit Akın';
+// $args1 = 20;
+// $args2 = 20;
+// print "<br>";
+// print "<h3>Koşullu ifadeler</h3>" . "<br>";
+// if ($args1 === $args2) {
+//     print "Eşittir";
+// } elseif ($args1 > $args2) {
+//     print $args1 . " Büyüktür...";
+// } elseif ($args1 < $args2) {
+//     print $args1 . " Küçüktür...";
+// } else {
+//     print "Böyle bir Koşul Yok";
+// }
+// print "<br>";
+// print "<h3>Diziler</h3>" . "<br>";
+
+
+// $array = [
+//     ["product" => "Telefon", "Marka" => "Samsung", "color" => "Siyah"],
+//     [ "product" => "Bilgisayar", "Marka" => "Huawei", "color" => "Siyah","İşlemci Tipi"=>["i7","RYZEN 7"] ],    
+//     ["product" => "Tablet", "Marka" => "Huawei", "color" => "Siyah"]
+// ];
+// print "<pre>";
+// print_r($array);
+// print "</pre>";
+// print "<br>";
+// print "<h3>Koşullu diziler 2</h3>" . "<br>";
+
+
+//  foreach ($array as $key => $value) {
+//   print   "Ürün Adı = ".$value['product']." ".$value['Marka']."<br>";
+
+
+//  }
+//  print "<h3>Koşullu diziler 3</h3>" . "<br>";
+//  $array2 = [
+//     "Bilgisayar",
+//     "Telefon"
+//  ];
+// for ($i=0; $i < count($array2) ; $i++) { 
+//     print  $array2[$i]."<br>";
+// }
+// print "<h3>Koşullu diziler 3 While</h3>" . "<br>";
+
+// $array3 = [
+//     "Bilgisayar",
+//     "Telefon",
+//     "Tablet",
+//     "Playstation",
+//     "Sırt Çantası",
+//     "Bilgisayar"
+// ];
+// $a = 0;
+// while ($a <= 5) {
+//     print $array3[$a]."<hr/>";
+//     $a++;
+// }
+// print "<h3>Ternary öperatörü</h3>" . "<br>";
+// $sayi1 = 80;
+// $sayi2 = 20;
+
+// ternary öperatörü
+// print ($sayi1 > $sayi2) ? 'Büyüktür' : 'Küçüktür';
+// print "<br>";
+// if ($sayi1 > $sayi2) {
+//     # code...
+//     print "Büyüktür";
+// }else {
+//   print "Küçüktür";
+// }
+
+
+// $sayi1 = null;
+// $sayi2 = 20;
+// null kontrol şekli 
+// print $sayi1 ?? 'Değersiz';
+// print $sayi1 ?: 'Değersiz';
+
+
+// array map foksiyonu dizinin içini boşaltma
+// $array4 = [
+//     "Bilgisayar",
+//     "Telefon",
+//     "Tablet",
+//     "Playstation",
+//     "Sırt Çantası",
+//     "Bilgisayar"
+// ];
+
+// $new_array = array_map(function ($item){
+// return "ürünler :" . $item;
+// },$array4);
+
+// var_dump($new_array);
+
+// $string = "Bilgisayar,Telefon,Kitap,Defter,Kalem";
+// $array = explode(",",$string);
+// print_r($array);
+
+// $array4 = [
+//         "Bilgisayar",
+//         "Telefon",
+//         "Tablet",
+//         "Playstation",
+//         "Sırt Çantası",
+//         "Bilgisayar"
+//     ];
+
+//     $string = implode("<=>",$array4);
+//     print_r($string);
+
+//** Deneme Tekrar Kısmı Aşagıda*/ 
 
 // $array = [
 //     "Product" =>
