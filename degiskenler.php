292db5ec7d71bb54a214382b8f73e2f64a9d4828
Mehmet Reşit Akın <103512411+resitakinn@users.noreply.github.com>
2024-11-13 22:36:54 +0300

    değişken-dosyası-oluşturuldu

---
+++ degiskenler.php
@@ -0,0 +1,28 @@
+<?php
+
+$GLOBALS;
+$_SERVER;
+$_REQUEST;
+$_FILES;
+$_ENV;
+$_SESSION;
+$_COOKIE;
+$_GET;
+$_POST;
+$http_response_header;
+$argc;
+$argv;
+
+// Değişkenler
+
+$string = 'ben bir değişkenim';
+$_variable = 'alt cizgi ile başlayabilir';
+//? $12Sayi = "Sayi ile başlayamam";
+
+$data =120;
+
+print $data; 
+
+$data = 130;
+print $data; 
+?>
